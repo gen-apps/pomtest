1f5591a598bec2f1dcd6d5c1b84769be36475e3a
geno <genad.papashvili@yahoo.com>
2022-08-28 23:50:34 +0400

    update

--- src/main/java/io/vom/appium/AppiumDriverImpl.java
+++ src/main/java/io/vom/appium/AppiumDriverImpl.java
@@ -85,7 +85,7 @@ public class AppiumDriverImpl implements Driver {
             }else{
                 appiumDriver = new AppiumDriver(url,caps);
             }
-            Duration implicitlyDuration = Duration.ofSeconds(Integer.parseInt(prop.getProperty("implicitly_wait_time_in_seconds", "0")));
+            Duration implicitlyDuration = Duration.ofSeconds(Long.parseLong(prop.getProperty("implicitly_wait_time_in_seconds", "0")));
             appiumDriver.manage().timeouts().implicitlyWait(implicitlyDuration);
         } catch (Exception e) {
             throw new RuntimeException(e);
